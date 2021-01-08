b3afeb1b36650007e880abe995aa6abf8c8bdec1
Tarodictrl <vudi600@gmail.com>
2021-01-09 00:11:39 +0700

    Updates

---
+++ precache-manifest.10295fe9564d46e5687862c8d1a4bd56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beabe8a9379a0945b872a1eedaf6a028",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0115c5a10696b33fe42",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "a8a6f5474abb3814b51d",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d0115c5a10696b33fe42",
+    "url": "./static/js/2.6c37df63.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.6c37df63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a6f5474abb3814b51d",
+    "url": "./static/js/main.a0379129.chunk.js"
+  },
+  {
+    "revision": "1c9b504a697469ddc397",
+    "url": "./static/js/runtime-main.7f5553f8.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
